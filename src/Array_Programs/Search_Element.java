81c719e48c1070ce6a91c2c4e779c0356d8c33f1
Pranay Dhoble <140973894+Pranay-Dhoble@users.noreply.github.com>
2023-08-01 01:20:06 +0530

    Create Search_Element.java
    Search Element in Array

---
+++ src/Array_Programs/Search_Element.java
@@ -0,0 +1,21 @@
+package Array_Programs;
+import java.util.*;
+public class Search_Element {
+    public static void main(String[] args) {
+        int []a = {20,50,70,80,40,20,4,60,3,8,5,10,9};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Find Number : ");
+        int val = sc.nextInt();
+        boolean present = false;
+
+        for(int i = 0; i < a.length; i++){
+            if(val == a[i]){
+               present = true;
+               System.out.println("Given Element is Present in Array and its Index Number is  :"+ i);
+            }
+        }
+        if(present == false){
+            System.out.println("Given Element is Absent in Array ");
+        }
+    }
+}
