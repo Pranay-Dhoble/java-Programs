2df67c089e3ba85b3a8aa7b557faf4cc44a91860
Pranay Dhoble <140973894+Pranay-Dhoble@users.noreply.github.com>
2023-08-01 00:54:22 +0530

    Create Max_Element.java
    Maximum Element of an Array  : 90;

---
+++ src/Array_Programs/Max_Element.java
@@ -0,0 +1,13 @@
+package Array_Programs;
+public class Max_Element {
+    public static void main(String[] args) {
+        int []a = {30,20,10,70,40,70,80,45,26,90};
+        int temp = a[0];
+        for(int i = 1; i < a.length; i++){
+            if (temp < a[i]){
+                temp = a[i];
+            }
+        }
+        System.out.println(temp);
+    }
+}
