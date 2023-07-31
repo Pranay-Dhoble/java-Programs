12303ec82982e5fa1fa39423354c0f8aeea0ba6d
Pranay Dhoble <140973894+Pranay-Dhoble@users.noreply.github.com>
2023-07-31 23:06:15 +0530

    Create Array_Palindrom_No.java
    Finding Palindrom Number From Array if  present

---
+++ src/Array_Programs/Array_Palindrom_No.java
@@ -0,0 +1,24 @@
+package Array_Programs;
+public class Array_Palindrom_No {
+    public static void main(String[] args) {
+        int a[] = {121, 345, 343, 769, 444};
+        int n = 0;
+        int p = 0;
+        for (int i = 0; i < a.length; i++){
+
+            int temp = a[i];
+            while (a[i] != 0) {
+                n = a[i] % 10;
+                p = (p * 10) + n;
+                a[i] = a[i] / 10;
+
+            }
+            if(temp == p){
+                System.out.println("Palimdrom Number is :"+temp);
+            }
+            p = 0;
+            n = 0;
+        }
+    }
+}
+
